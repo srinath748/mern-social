43b8e2e2f89f660c604e3304a5790e461678a079
srinath748 <msrinath748@gmail.com>
2025-09-03 22:05:54 +0530

    update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// Use deployed backend URL
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `https://mern-social-1-q5t5.onrender.com/api`, // deployed backend URL
 });
 
+// Attach token to every request if available
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
